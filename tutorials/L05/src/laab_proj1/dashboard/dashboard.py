c6ee9fe9887376a40472f59c7746cc6a484dc68a
zofika <zofia.dobrek@interia.com>
2026-08-18 16:55:00 +0200

    L0123 and beginning of L045

--- tutorials/L05/src/laab_proj1/dashboard/dashboard.py
+++ tutorials/L05/src/laab_proj1/dashboard/dashboard.py
@@ -2,10 +2,10 @@ from flask import Flask, Blueprint
 
 app = Flask(__name__)
 
-from .app1 import routes as app1_routes
+from app1 import routes as app1_routes
 app.register_blueprint(app1_routes.bp, url_prefix='/')
 
-from .app2 import routes as app2_routes
+from app2 import routes as app2_routes
 app.register_blueprint(app2_routes.bp, url_prefix='/app2')
 
 
---
+++ tutorials/new_simple_webpage/README.md
@@ -0,0 +1,1 @@
+# New Simple Webpage tutorial learning
