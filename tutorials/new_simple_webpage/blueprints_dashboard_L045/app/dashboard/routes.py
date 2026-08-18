584400c764883fbd7fccd8a288930e69ee5108ea
zofika <zofia.dobrek@interia.com>
2026-08-18 17:45:53 +0200

    changes towards L045

---
+++ tutorials/new_simple_webpage/blueprints_dashboard_L045/app/dashboard/routes.py
@@ -0,0 +1,11 @@
+from flask import Blueprint
+
+dashboard_bp = Blueprint(
+    "dashboard", 
+    __name__, 
+    url_prefix = "/dashboard"
+    )
+
+@dashboard_bp.route("/")
+def index():
+    return "Dashboard"
--- tutorials/new_simple_webpage/blueprints_dashboard_L045/dashboard.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, Blueprint
-
-app = Flask(__name__)
-
-from .app1 import routes as app1_routes
-app.register_blueprint(app1_routes.bp, url_prefix='/')
-
-from .app2 import routes as app2_routes
-app.register_blueprint(app2_routes.bp, url_prefix='/app2')
