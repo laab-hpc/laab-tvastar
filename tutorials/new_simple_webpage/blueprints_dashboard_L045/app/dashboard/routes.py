8a65909c3ef737bad8742c2a84bb674feda82ea6
zofika <zofia.dobrek@interia.com>
2026-08-19 14:17:22 +0200

    dashboard page

--- tutorials/new_simple_webpage/blueprints_dashboard_L045/app/dashboard/routes.py
+++ tutorials/new_simple_webpage/blueprints_dashboard_L045/app/dashboard/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 dashboard_bp = Blueprint(
     "dashboard", 
@@ -8,4 +8,25 @@ dashboard_bp = Blueprint(
 
 @dashboard_bp.route("/")
 def index():
-    return "Dashboard"
+    head = "Dashboard"
+    title = "Performance Report"
+    version = 1.0
+    libraries = [
+        "OpenBLAS",
+        "MKL",
+        "AOCL"
+        ]
+    results = [
+        {"library": "OpenBLAS", "gflops": 120},
+        {"library": "MKL",      "gflops": 145},
+        {"library": "AOCL",     "gflops": 138}
+        ]
+    
+    return render_template(
+        "dashboard/index.html",
+        head=head,
+        title=title,
+        version=version,
+        libraries=libraries,
+        results=results
+        )
